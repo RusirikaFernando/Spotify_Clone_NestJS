17f8901828d5b480334a1c2d2439f2ef0b063dad
RusirikaFernando <thathsaranif7@gmail.com>
2024-08-16 11:13:32 +0530

    update CreateSongDTO by adding Lyrics lyrics isadded as a optional string

--- src/songs/dto/create-song-dto.ts
+++ src/songs/dto/create-song-dto.ts
@@ -1,24 +1,34 @@
 //DTO = its like a blue print. data transfer object. use decorators to validate objects that goes thoru the db. helps to catch errors without wasting time. is spesifies that when the error is on variables.
 // the the dto is very usefull to catch errors.
 
-import {  IsArray, IsDateString, IsMilitaryTime, IsNotEmpty, IsString } from "class-validator";
+import {
+  IsArray,
+  IsDateString,
+  IsMilitaryTime,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
-export class CreateSongDTO{
+export class CreateSongDTO {
+  @IsString()
+  @IsNotEmpty()
+  readonly title: string;
 
-    @IsString()
-    @IsNotEmpty()
-    readonly title: string;
+  @IsNotEmpty()
+  @IsArray()
+  @IsString({ each: true })
+  readonly artists: string[];
 
-    @IsNotEmpty()
-    @IsArray()
-    @IsString({each:true})
-    readonly artists: string[];
+  @IsNotEmpty()
+  @IsDateString()
+  readonly releasedDate: Date;
 
-    @IsNotEmpty()
-    @IsDateString()
-    readonly releasedDate: Date;
+  @IsMilitaryTime()
+  @IsNotEmpty()
+  readonly duration: Date;
 
-    @IsMilitaryTime()
-    @IsNotEmpty()
-    readonly duration: Date;
+  @IsString()
+  @IsOptional()
+  readonly lyrics: string;
 }
